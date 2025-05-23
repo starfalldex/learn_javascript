902a4a2bb7e4f33c2cc3ee0e5f45872b0bc5f0b5
starfalldex <starfalldex@gmail.com>
2025-05-23 14:06:35 +0530

    add: 03: operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,11 @@
 let score = 33;
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let inNumber = Number(score);
-console.log("inNumber:", typeof inNumber, inNumber)
+// console.log("inNumber:", typeof inNumber, inNumber)
+// -------------------------
 
 // Number()
 // "33" >> 33
@@ -12,24 +13,55 @@ console.log("inNumber:", typeof inNumber, inNumber)
 // true >> 1; false >> 0;
 // undefined >> NaN
 // null >> 0
+// --------------------------------------------------
+
 
 let isLoggedIn = 0;
 
 let inBoolean = Boolean(isLoggedIn)
-console.log("inBoolean:", typeof inBoolean, inBoolean)
+// console.log("inBoolean:", typeof inBoolean, inBoolean)
+// -------------------------
 
 // Boolean()
 // 0 >> false; 1 >> true; any +ve,-ve number >> true;
 // "" >> false; "notEmpty" >> true
 // undefined >> false
 // null >> false
+// --------------------------------------------------
+
 
 let someDataType = 34
 
 let inString = String(someDataType)
-console.log("inString:", typeof inString, inString)
+// console.log("inString:", typeof inString, inString)
+// -------------------------
 
 // String()
 // 44 >> "44"
 // null >> null
 // undefined >> undefined
+// --------------------------------------------------
+
+
+// Operations
+// +,-,*,/,**,%
+
+let str = "starfall" + " " + "dex";
+// console.log(str);
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// -------------------------
+
+// dont write these type of code
+// console.log(+true);
+// console.log(+false);
+// console.log(-true);
+// console.log(-false);
+// console.log(+"");
+
+// let a = 1;
+// let b = a++;
+// let c = ++a;
+// console.log(a,b,c)

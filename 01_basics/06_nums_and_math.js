8ce226be0c423c98d160dd2ea8b57c305f42df6d
starfalldex <starfalldex@gmail.com>
2025-05-23 16:28:10 +0530

    add: 06: nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 500;
+// other way to declare number
+const balance = new Number(9999);
+
+// console.log(score, balance)
+// console.log(typeof score, typeof balance)
+
+// console.log(typeof balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// -------------------------
+
+const otherBalance = 1456.9966
+// console.log(otherBalance.toPrecision(4))
+
+const hundereds = 1000000000
+// console.log(hundereds.toLocaleString()) 
+// console.log(hundereds.toLocaleString("en-IN")) 
+// --------------------------------------------------
+
+// Maths
+// console.log(Math.abs(-4545))
+// console.log(Math.round(4.3))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.1))
+
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(5, 3))
+// console.log(Math.min(4, 7, 7, 2))
+// console.log(Math.max(4, 7, 7, 2))
+
+// console.log(Math.random());
+// console.log((Math.random() * 4) + 1);
+// console.log(Math.floor((Math.random() * 4) + 1));
+
+const min = 5;
+const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// -------------------------

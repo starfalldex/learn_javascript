39882662836693cfc75e9ee3cbe3eb04deb28b5a
starfalldex <starfalldex@gmail.com>
2025-05-24 23:09:54 +0530

    add: 07

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,9 @@
+// in future the date will be converting to temporal
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
